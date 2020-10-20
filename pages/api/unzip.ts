e8d1fec9407a69120aecbebdf91ed9a50fb5eada
Gal Schlezinger <gal@spitfire.co.il>
2020-10-20 14:16:53 +0300

    small implementation

---
+++ pages/api/unzip.ts
@@ -0,0 +1,21 @@
+import { NowRequest, NowResponse } from "@vercel/node";
+import { pipeFileToResponse } from "../../src/unzip";
+import {validateUrl} from "../../src/validateUrl";
+
+export default async function UnzipApi(req: NowRequest, res: NowResponse) {
+  const { archive, path } = req.query;
+
+  if (typeof archive !== "string") {
+    res.status(400).send({ error: "expected `archive` param as string" });
+    return;
+  }
+
+  if (typeof path !== "string") {
+    res.status(400).send({ error: "expected `path` query param as string" });
+    return;
+  }
+
+  validateUrl(archive);
+  res.setHeader("cache-control", "stale-while-revalidate");
+  pipeFileToResponse(archive, path, res);
+}
---
+++ pages/index.tsx
@@ -0,0 +1,72 @@
+import { GetServerSideProps } from "next";
+import { File, listFilesInZip } from "../src/unzip";
+import {validateUrl} from "../src/validateUrl";
+
+type Props =
+  | {
+      state: "listFiles";
+      archive: string;
+      files: File[];
+    }
+  | {
+      state: "home";
+    };
+
+export default function Home(props: Props): NonNullable<React.ReactNode> {
+  switch (props.state) {
+    case "home": {
+      return <div>Hello, world! {JSON.stringify(props)}</div>;
+    }
+    case "listFiles": {
+      return (
+        <div>
+          <h1>
+            Files in <code>{props.archive}</code>
+          </h1>
+          <ul>
+            {props.files
+              .filter((f) => f.type === "File")
+              .map((file) => {
+                const path = encodeURIComponent(file.fileName);
+                const archive = encodeURIComponent(props.archive);
+                return (
+                  <li key={file.fileName}>
+                    <a href={`/api/unzip?path=${path}&archive=${archive}`}>
+                      <code>{file.fileName}</code>
+                    </a>
+                  </li>
+                );
+              })}
+          </ul>
+        </div>
+      );
+    }
+  }
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
+  const { archive } = ctx.query;
+
+  if (archive) {
+    if (typeof archive !== "string") {
+      throw new Error("archive must be a string");
+    }
+
+    validateUrl(archive);
+    const files = await listFilesInZip(archive);
+
+    return {
+      props: {
+        state: "listFiles",
+        archive,
+        files,
+      },
+    };
+  }
+
+  return {
+    props: {
+      state: "home",
+    },
+  };
+};
