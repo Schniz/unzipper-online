c6c8bafa5d2d682935950b2057896d54376341a7
Gal Schlezinger <gal@spitfire.co.il>
2020-10-20 14:48:26 +0300

    mo error handling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
 import { GetServerSideProps } from "next";
 import { File, listFilesInZip } from "../src/unzip";
-import {validateUrl} from "../src/validateUrl";
+import { validateUrl } from "../src/validateUrl";
+import { useRef } from "react";
+import { useRouter } from "next/router";
 
 type Props =
+  | {
+      state: "errorReading";
+      message: string;
+      archive: string;
+    }
   | {
       state: "listFiles";
       archive: string;
@@ -13,13 +20,43 @@ type Props =
     };
 
 export default function Home(props: Props): NonNullable<React.ReactNode> {
-  switch (props.state) {
-    case "home": {
-      return <div>Hello, world! {JSON.stringify(props)}</div>;
-    }
-    case "listFiles": {
-      return (
-        <div>
+  const textInputRef = useRef<HTMLInputElement>(null);
+  const router = useRouter();
+
+  return (
+    <div>
+      See files in zips
+      <form
+        action="/"
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (!textInputRef.current) return;
+          const archive = encodeURIComponent(textInputRef.current.value.trim());
+          router.push(`/?archive=${archive}`);
+        }}
+      >
+        <input
+          name="archive"
+          ref={textInputRef}
+          type="url"
+          defaultValue={
+            props.state === "listFiles"
+              ? props.archive
+              : props.state === "errorReading"
+              ? props.archive
+              : ""
+          }
+          placeholder="https://example.com/file.zip"
+        />
+      </form>
+      {props.state === "errorReading" && (
+        <>
+          <h1>An error occured</h1>
+          <p>{props.message}</p>
+        </>
+      )}
+      {props.state === "listFiles" && (
+        <>
           <h1>
             Files in <code>{props.archive}</code>
           </h1>
@@ -38,10 +75,10 @@ export default function Home(props: Props): NonNullable<React.ReactNode> {
                 );
               })}
           </ul>
-        </div>
-      );
-    }
-  }
+        </>
+      )}
+    </div>
+  );
 }
 
 export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
@@ -53,16 +90,26 @@ export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
     }
 
     validateUrl(archive);
-    ctx.res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate');
-    const files = await listFilesInZip(archive);
+    ctx.res.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate");
+    try {
+      const files = await listFilesInZip(archive);
 
-    return {
-      props: {
-        state: "listFiles",
-        archive,
-        files,
-      },
-    };
+      return {
+        props: {
+          state: "listFiles",
+          archive,
+          files,
+        },
+      };
+    } catch (e) {
+      return {
+        props: {
+          state: 'errorReading',
+          archive,
+          message: e.message,
+        }
+      }
+    }
   }
 
   return {
